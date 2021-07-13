e82bbf6a73de5d7c1ed5519df8585fa217c776aa
Qprah <62654357+Qprah@users.noreply.github.com>
2021-07-14 02:35:23 +0530

    phase 3 king - working rough, only valid moves, not color checking phase 3 pawn - working good, only valid moves, color checking commented for now!

--- Pawn.java
+++ Pawn.java
@@ -12,4 +12,41 @@ public class Pawn extends ChessPiece {
         super.setGraphics("PawnBlack.png", "PawnWhite.png");
 
     }
+    public boolean isValidMove(int currentRow, int currentCol, int futureRow, int futureCol) {
+
+        if(futureRow == currentRow && futureCol == currentRow) {
+            return false;
+        }
+        if( super.getBoard().getPieceColor(currentRow,currentCol) == "WHITE" && futureRow == currentRow+1 && futureCol == currentCol){
+            return true;
+        }
+        if( super.getBoard().getPieceColor(currentRow,currentCol) == "BLACK" && futureRow == currentRow-1 && futureCol == currentCol){
+            return true;
+        }
+//        for(int i = 0 ; i<currentRow+2 ; i++){
+//            for(int j = 0 ; j<currentRow+2 ; j++){
+//                if(super.getBoard().hasPiece(i,j)){
+//                    if(super.getBoard().getPieceColor(i,j) == super.getBoard().getPieceColor(currentRow,currentCol)){
+//                        return true;
+//                    }
+//                }
+//            }
+//        }
+
+
+
+
+
+
+
+
+
+//                if(super.getBoard().getPieceColor(currentRow, currentCol)
+//                        == super.getBoard().getPieceColor(7, 4)){
+//                    return false;
+//                }
+
+        return false;
+    }
 }
+
