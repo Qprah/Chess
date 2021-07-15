039aab079a8cc353d29a28fc1368be1f6fea9ebd
Qprah <62654357+Qprah@users.noreply.github.com>
2021-07-15 20:11:52 +0530

    Pawn working properly except KILLING DIAGONALLY

--- Pawn.java
+++ Pawn.java
@@ -13,34 +13,60 @@ public class Pawn extends ChessPiece {
     }
 
     public boolean isValidMove(int currentRow, int currentCol, int futureRow, int futureCol) {
+/**
+ * PAWN features:
+ *
+ *   ----- Moves two steps at first turn and then only one step.
+ *
+ *   ----- Can't kill the opposite in it's own column
+ *
+ *   ----- Kills diagonally (can ignore and move forward or it can kill diagonally)
+ *
+ */
 
-        if ((getBoard().getPieceColor(futureRow, futureCol)).equals(getBoard().getPieceColor(currentRow, currentCol)))
-            return false;
-
-        // white piece - lets the white pawn take two steps initially
-        if (super.getBoard().getPieceColor(currentRow, currentCol).equals(Board.white)) {
-            if ((futureRow == currentRow + 2 || futureRow == currentRow + 1) && futureCol == currentCol) {
-                return true;
+        //TWO STEPS CODE ---------------------------------------------------------------------------------------------------------------------------
+        //only at row 1 and when the piece is white .. it will move two steps
+        if (currentRow == 1 && getBoard().getPieceColor(currentRow, currentCol).equals(Board.white)) {  // have two steps if It's in row 6 or Row 1
+            int num = 2;  //more work required
+            for (int i = 1; i <= num; i++) {
+                if (getBoard().hasPiece(currentRow + i, currentCol)) {  // if any piece in its column, it won't be able to kill it.
+                    return false;
+                } else if (futureRow == currentRow + i && futureCol == currentCol) {//iteration for making the pawn go up 2 tiles
+                    return true;
+                }
             }
         }
+        //only when at row 6 and the piece is black .. it will move two steps down
+        else if (currentRow == 6 && getBoard().getPieceColor(currentRow, currentCol).equals(Board.black)) {
+            int num = 2;  //more work required
+            for (int i = 1; i <= num; i++) {
+                if (getBoard().hasPiece(currentRow - i, currentCol)) { // if any piece in its column then it won't kill.
+                    return false;
+                } else if (futureRow == currentRow - i && futureCol == currentCol) {//iteration for making the pawn go up 2 tiles
+                    return true;
+                }
+            }
+            // ONE STEP CODE --------------------------------------------------------------------------------------------------------------
+        } else if (currentRow != 1 && getBoard().getPieceColor(currentRow, currentCol).equals(Board.white)) {
 
-        // black piece - lets the black pawn take two steps initially
-        if (super.getBoard().getPieceColor(currentRow, currentCol).equals(Board.black)) {
-            return (futureRow == currentRow - 2 || futureRow == currentRow - 1) && futureCol == currentCol;
+            //iteration for making the pawn go up 1 tile
+            if (getBoard().hasPiece(currentRow + 1, currentCol)) { // even when moving one step, it can't kill
+                return false;
+            } else return futureRow == currentRow + 1 && futureCol == currentCol;
+
+        } else if (currentRow != 1 && getBoard().getPieceColor(currentRow, currentCol).equals(Board.black)) {
+            int num = 1;  //more work required
+            for (int i = 1; i <= num; i++) {
+                if (getBoard().hasPiece(currentRow - i, currentCol)) { // even when moving one step it won't kill
+                    return false;
+                } else if (futureRow == currentRow - i && futureCol == currentCol) {//iteration for making the pawn go down 1 tile
+                    return true;
+                }
+            }
         }
-//        System.out.println(super.getBoard().getPieceColor(currentRow, currentCol) + " "+
-//        super.getBoard().getPieceColor(futureRow, futureCol)+ "    fr  " + futureRow + "    fc " + futureCol
-//        + "    cr "+ currentRow + "    cc "+currentCol);
-
         return false;
     }
 }
-
-//                if(super.getBoard().getPieceColor(currentRow, currentCol)
-//                        == super.getBoard().getPieceColor(7, 4)){
-//                    return false;
-//                }
-
 
 
 
