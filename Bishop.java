21525dfeac052baca1e54049c2fe957ea140dcd0
Qprah <62654357+Qprah@users.noreply.github.com>
2021-07-17 19:01:46 +0530

    Bishop refined, valid moves are being blocked but BR AND TL diagonals are yet to complete!

--- Bishop.java
+++ Bishop.java
@@ -1,3 +1,5 @@
+import javax.swing.plaf.ColorUIResource;
+
 public class Bishop extends ChessPiece {
 
     public Bishop(String newColor, Board theBoard) {
@@ -17,48 +19,103 @@ public class Bishop extends ChessPiece {
             return false;
         }
 
-        /**
-         * The do-while loops are responsible for the diagonal movement of the bishop.
-         *
-         * */
-        // diagonal in top-right direction.
-        int i = 0;
-        do {
-            if (currentRow + i == futureRow && currentCol + i == futureCol) {
-                return true;
+        // For top-right direction
+        for (int i = 1; i <= maxIterationsTopRight(currentRow, currentCol); i++) {
+
+                if (getBoard().hasPiece(currentRow + i, currentCol + i)) {
+                    break;
+                }
+                else {
+                    if (currentRow + i == futureRow && currentCol + i == futureCol) {
+                        return true;
+                    }
+
+                }
             }
-            i++;
-        } while ((i < 10));
 
-        // diagonal in bottom-left direction
-        int j = 0;
-        do {
-            if (currentRow - j == futureRow && currentCol - j == futureCol) {
-                return true;
+        for (int i = 1; i <= maxIterationsBottomLeft(currentRow, currentCol); i++) {
+            if (getBoard().hasPiece(currentRow - i, currentCol - i)) {
+                break;
             }
-            j++;
-        } while ((j < 10));
+            else {
+                if (currentRow - i == futureRow && currentCol - i == futureCol) {
+                    return true;
+                }
 
-        // diagonal in top-left direction
-        int k = 0;
-        do {
-            if (currentRow + k == futureRow && currentCol - k == futureCol) {
-                return true;
             }
-            k++;
-        } while ((k < 10));
-
-        // diagonal in bottom-right direction
-        int l = 0;
-        do {
-            if (currentRow - l == futureRow && currentCol + l == futureCol) {
-                return true;
+        }
+        for(int i = 1; i<Board.BOARD_SIZE-1 ; i++)
+            if (getBoard().hasPiece(currentRow - i, currentCol + i)) {
+                break;
             }
-            l++;
-        } while ((l < 10));
+            else {
+                if (currentRow - i == futureRow && currentCol + i == futureCol) {
+                    return true;
+                }
 
-
+            }
         return false;
+    }
+
+
+    /**
+     * The do-while loops are responsible for the diagonal movement of the bishop.
+     */
+//        // diagonal in top-right direction.
+//        int i = 0;
+//        do {
+//            if (currentRow + i == futureRow && currentCol + i == futureCol) {
+//                return true;
+//            }
+//            i++;
+//        } while ((i < 10));
+//
+//        // diagonal in bottom-left direction
+//        int j = 0;
+//        do {
+//            if (currentRow - j == futureRow && currentCol - j == futureCol) {
+//                return true;
+//            }
+//            j++;
+//        } while ((j < 10));
+//
+//        // diagonal in top-left direction
+//        int k = 0;
+//        do {
+//            if (currentRow + k == futureRow && currentCol - k == futureCol) {
+//                return true;
+//            }
+//            k++;
+//        } while ((k < 10));
+//
+//        // diagonal in bottom-right direction
+//        int l = 0;
+//        do {
+//            if (currentRow - l == futureRow && currentCol + l == futureCol) {
+//                return true;
+//            }
+//            l++;
+//        } while ((l < 10));
+//
+//
+//        return false;
+//    }
+    public int maxIterationsTopRight(int currentRow, int currentCol) {
+        int iterations;
+        iterations = Math.min((Board.BOARD_SIZE-1) - currentRow,(Board.BOARD_SIZE-1) - currentCol);
+        return iterations;
+    }
+    public int maxIterationsBottomLeft(int currentRow, int currentCol) {
+        int iterations;
+
+        iterations = Math.min(currentRow,currentCol);
+        return iterations;
+    }
+    public int maxIterationsBottomRight(int currentRow, int currentCol) {
+        int iterations=3;
+
+        iterations = (Board.BOARD_SIZE-1) - Math.max(currentRow+1, currentCol+1);
+        return iterations;
     }
 
     /**
--- TestClassA3.java
+++ TestClassA3.java
@@ -32,39 +32,5 @@ public class TestClassA3 {
         b.drawBoard();
         BoardLoader.loadBoardState(b,BoardLoader.folder + BoardLoader.fileName);
     }
-
-    // Should be a board with one red and one black tile in the corners (see images. )
-//    public static void phase1() {
-//        Board b = new Board();
-//
-//        System.out.println("Attempt 1 - Error - No Tiles: ");
-//        b.drawBoard();
-//        b.drawPiece(1, 1);
-//        b.drawPiece(6, 6);
-//
-//
-//        // This should fail since there is no pieces on the board yet and drawPiece draws the piece.
-//        try {
-//            System.out.println("Attempt 2 - (1,1) (6,6) Corner Tiles: OOPS - it should be BLACK or Board.black, not Black ");
-//            b.setPiece(1, 1, new Pawn("Pawn1", "Black", b));
-//            b.setPiece(6, 6, new Pawn("Pawn2", "Whites", b));
-//            b.drawPiece(1, 1);
-//            b.drawPiece(6, 6);
-//        } catch (Exception e) {
-//            System.out.println(e);
-//            e.printStackTrace();
-//        }
-//
-//
-//        b.setPiece(1, 1, new Pawn("Pawn3", "BLACK", b));
-//        b.setPiece(6, 6, new Pawn("Pawn4", "WHITE", b));
-//        b.drawPiece(1, 1);
-//        b.drawPiece(6, 6);
-//
-//        b.setPiece(2, 2, new Pawn("Pawn5", "BLACK", b));
-//        b.setPiece(5, 5, new Pawn("Pawn6", "WHITE", b));
-//        b.drawPieces();
-//
-//    }
 }
 
